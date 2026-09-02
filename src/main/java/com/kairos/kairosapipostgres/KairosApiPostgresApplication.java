dce6adae8e396f61bf083e46f2ae6bb4743b655f
pjz3650 <d.dias3650@gmail.com>
2026-09-02 12:28:57 -0300

    feat: initialize project structure with Spring Boot, add configuration files, and set up dependencies

---
+++ src/main/java/com/kairos/kairosapipostgres/KairosApiPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.kairos.kairosapipostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KairosApiPostgresApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (KairosApiPostgresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kairos/kairosapipostgres/KairosApiPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairos.kairosapipostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairosApiPostgresApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
